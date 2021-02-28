f07bd61b4c13244f28272261384a1a5053faae3f
Roman Dobrodii <romariosi007@gmail.com>
2021-02-28 17:26:34 +0200

    With createAction()

--- src/redux/contacts/contactsActions.js
+++ src/redux/contacts/contactsActions.js
@@ -1,28 +1,30 @@
 import { v4 as uuidv4 } from 'uuid';
+import { createAction } from '@reduxjs/toolkit';
 import actionTypes from './contactsActionTypes';
 
-const addContact = ({ name, number }) => ({
-  type: actionTypes.ADD,
-  payload: {
-    id: uuidv4(),
-    name,
-    number,
-  },
+const addContact = createAction(actionTypes.ADD, ({ name, number }) => {
+  return {
+    payload: {
+      id: uuidv4(),
+      name,
+      number,
+    },
+  };
 });
 
-const deleteContact = id => ({
-  type: actionTypes.DELETE,
-  payload: { id },
+const deleteContact = createAction(actionTypes.DELETE, id => {
+  return {
+    payload: { id },
+  };
 });
 
-const changeFilter = filter => ({
-  type: actionTypes.CHANGE_FILTER,
-  payload: { filter },
+const changeFilter = createAction(actionTypes.CHANGE_FILTER, filter => {
+  return {
+    payload: { filter },
+  };
 });
 
-const showAlert = () => ({
-  type: actionTypes.ALERT,
-});
+const showAlert = createAction(actionTypes.ALERT);
 
 export default {
   addContact,
@@ -30,3 +32,28 @@ export default {
   changeFilter,
   showAlert,
 };
+
+// function addContact({ name, number }) {
+//   return ({
+//     type: actionTypes.ADD,
+//     payload: {
+//       id: uuidv4(),
+//       name,
+//       number,
+//     },
+//   });
+// }
+
+// const deleteContact = id => ({
+//   type: actionTypes.DELETE,
+//   payload: { id },
+// });
+
+// const changeFilter = filter => ({
+//   type: actionTypes.CHANGE_FILTER,
+//   payload: { filter },
+// });
+
+// const showAlert = () => ({
+//   type: actionTypes.ALERT,
+// });
